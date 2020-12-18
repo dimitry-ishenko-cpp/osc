5c221ef8352222fbd92a758d6a3dd40b07bbb7c1
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-12-18 15:51:06 -0500

    packet: make mutable

--- packet.hpp
+++ packet.hpp
@@ -24,11 +24,34 @@ class packet
 {
 public:
     packet() = default;
+    explicit packet(int32 size) : data_(size) { }
     packet(const char* data, int32 size) : packet(data, data + size) { }
     packet(const char* begin, const char* end) : data_(begin, end) { }
 
     const char* data() const { return data_.data(); }
+    char* data() { return data_.data(); }
+
     int32 size() const { return data_.size(); }
+    void resize(int32 size) { data_.resize(size); }
+    void clear() { data_.clear(); }
+
+    auto begin() const { return data_.begin(); }
+    auto begin() { return data_.begin(); }
+
+    auto end() const { return data_.end(); }
+    auto end() { return data_.end(); }
+
+    auto cbegin() const { return data_.cbegin(); }
+    auto cend() const { return data_.cend(); }
+
+    auto rbegin() const { return data_.rbegin(); }
+    auto rbegin() { return data_.rbegin(); }
+
+    auto rend() const { return data_.rend(); }
+    auto rend() { return data_.rend(); }
+
+    auto crbegin() const { return data_.crbegin(); }
+    auto crend() const { return data_.crend(); }
 
     element parse();
 
