e38ad3f28f4995ebde2ffce15d511a5e49c2b3af
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-01-21 11:11:58 -0500

    errors: refactor

--- errors.hpp
+++ errors.hpp
@@ -16,40 +16,50 @@ namespace osc
 class invalid_value : public std::invalid_argument
 {
 public:
-    invalid_value() : std::invalid_argument("Invalid value") { }
-    explicit invalid_value(const std::string& why) : std::invalid_argument("Invalid value - " + why) { }
+    explicit invalid_value(const std::string& why) :
+        std::invalid_argument{ "Invalid value - " + why }
+    { }
+    invalid_value() : std::invalid_argument{ "Invalid value" } { }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 class invalid_message : public std::invalid_argument
 {
 public:
-    invalid_message() : std::invalid_argument("Invalid message") { }
-    explicit invalid_message(const std::string& why) : std::invalid_argument("Invalid message - " + why) { }
+    explicit invalid_message(const std::string& why) :
+        std::invalid_argument{ "Invalid message - " + why }
+    { }
+    invalid_message() : std::invalid_argument{ "Invalid message" } { }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 class invalid_element : public std::invalid_argument
 {
 public:
-    invalid_element() : std::invalid_argument("Invalid element") { }
-    explicit invalid_element(const std::string& why) : std::invalid_argument("Invalid element - " + why) { }
+    explicit invalid_element(const std::string& why) :
+        std::invalid_argument{ "Invalid element - " + why }
+    { }
+    invalid_element() : std::invalid_argument{ "Invalid element" } { }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 class invalid_bundle : public std::invalid_argument
 {
 public:
-    invalid_bundle() : std::invalid_argument("Invalid bundle") { }
-    explicit invalid_bundle(const std::string& why) : std::invalid_argument("Invalid bundle - " + why) { }
+    explicit invalid_bundle(const std::string& why) :
+        std::invalid_argument{ "Invalid bundle - " + why }
+    { }
+    invalid_bundle() : std::invalid_argument{ "Invalid bundle" } { }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 class invalid_packet : public std::invalid_argument
 {
 public:
-    invalid_packet() : std::invalid_argument("Invalid packet") { }
-    explicit invalid_packet(const std::string& why) : std::invalid_argument("Invalid packet - " + why) { }
+    explicit invalid_packet(const std::string& why) :
+        std::invalid_argument{ "Invalid packet - " + why }
+    { }
+    invalid_packet() : std::invalid_argument{ "Invalid packet" } { }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
